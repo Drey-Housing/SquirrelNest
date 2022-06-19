87d591bbcb7cdc6fb498bed67825bd4ec3237220
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2022-06-19 11:50:54 -0700

    added a test

--- src/app/__Tests__/App.test.js
+++ src/app/__Tests__/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { test, expect } from '@jest/globals';
+import '@testing-library/jest-dom';
+import App from '../components/App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders App to DOM', () => {
+  render(<App />);
+  const appContainer = screen.getByTestId('app');
+  expect(appContainer).toBeInTheDocument();
 });
