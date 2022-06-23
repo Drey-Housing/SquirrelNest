beedd1f9a89ab144b29e68be8f60117eb4907603
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2022-06-22 21:50:36 -0700

    unfinished commit.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import { ThemeProvider } from '@mui/material/styles';
+import ProfileProvider from './components/Profile/ProfileContext';
 import CssBaseline from '@mui/material/CssBaseline';
 // import '../App.css';
+import {
+  Home, Dashboard, LogIn, SignUp, Jobs, Profile,
+} from './pages';
 import { useWindowSize } from './utils/customHooks';
 import NavBar from './components/NavBar';
+import Theme from './Theme';
 import authUtils from './utils/authUtils';
 
 
@@ -20,22 +25,53 @@ function App() {
     });
   }, []);
 
+  const newLogIn = (cb) => {
+    authUtils.getUser()
+      .then((res) => {
+        setLoggedIn(res.data.loggedIn);
+        if (!res.data.loggedIn) {
+          alert('Invalid username or password');
+        } else if (typeof (cb) === 'function') {
+          cb(res.data.loggedIn);
+        }
+      });
+  };
+
+  const nav = (val) => {
+    if (val) {
+      navigate('/dashboard');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div data-testid="app">
+      <CssBaseline />
+      <ThemeProvider theme={Theme}>
+        <ProfileProvider>
+          <Grid container direction="column" sx={{ width: '100vw', minHeight: '100vh' }}>
+            <NavBar loggedIn={loggedIn} />
 
-        <p>
-          SquirrelNest
-        </p>
-        <a
-          className="App-link"
-
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Properties Portfolio
-        </a>
-      </header>
+            <Routes>
+              <Route path="/" element={<Home newLogIn={newLogIn} nav={nav} />} exact />
+              <Route path="/signup" element={width < 800 ? <SignUp /> : <Home createAccount />} exact />
+              <Route path="/login" element={width < 800 ? <LogIn /> : <Home newLogIn={newLogIn} />} exact />
+              <Route path="/dashboard" element={<Dashboard />} exact />
+              <Route
+                path="/profile"
+                element={loggedIn ? <Profile /> : <Home newLogIn={newLogIn} />}
+                exact
+              />
+              <Route
+                path="/jobs"
+                element={
+                  loggedIn ? <Jobs /> : <Home newLogIn={newLogIn} />
+                }
+                exact
+              />
+            </Routes>
+          </Grid>
+        </ProfileProvider>
+      </ThemeProvider>
     </div>
   );
 }
