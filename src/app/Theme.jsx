949053531ca840fe606b8253caafd4f9a9b3587d
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2022-08-01 13:05:16 -0700

    log in screen up/ styling affected

--- src/app/Theme.jsx
+++ src/app/Theme.jsx
@@ -3,11 +3,11 @@ import { createTheme } from '@mui/material/styles';
 export default createTheme({
   palette: {
     primary: {
-      main: '#EDFEFF',
-      contrastText: '#4A485B',
+      main: '#A0835C',
+      contrastText: '#737E80',
     },
     secondary: { // works
-      main: '#4A485B',
+      main: '#737E80',
       contrastText: '#fff',
     },
     xiketic: {
